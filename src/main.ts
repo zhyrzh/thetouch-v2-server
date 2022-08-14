6c1ca1b3e047eda12821dd5bb5642e68105ed761
unknown <arongrhyz@gmail.com>
2022-08-15 00:09:16 +0800

    added body limit, validation pipe and cors origin domain

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,19 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'body-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  const app = await NestFactory.create(AppModule, {
+    rawBody: true,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.use(json({ limit: '50mb' }));
+  app.enableCors({ origin: 'http://127.0.0.1:5173' });
   await app.listen(5000);
 }
 bootstrap();
