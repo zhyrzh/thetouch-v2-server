2d3b591a71f5d6926f8d7e6df2da2d45454c26ae
unknown <arongrhyz@gmail.com>
2022-08-28 02:49:14 +0800

    added auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ArticlesModule } from './articles/articles.module';
 import { Journalists } from './journalists/journalists.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ArticlesModule, Journalists, PrismaModule],
+  imports: [ArticlesModule, Journalists, PrismaModule, AuthModule],
 })
 export class AppModule {}
